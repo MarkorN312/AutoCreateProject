01bf95847415ddc3ff62c2f388966af5ad4b5a10
MarkorN312 <45149444+MarkorN312@users.noreply.github.com>
2020-08-06 19:28:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ from github import Github
 
 username = sys.argv[2]
 password = sys.argv[3]
+path = 'path/to/saving/repository'
 
 def create():
     folderName = str(sys.argv[1])
-    os.makedirs(insert_path' + folderName)
+    os.makedirs(path + folderName)
     Github(username, password).get_user().create_repo(folderName)
     print('Succesfully created repository',folderName)
 
